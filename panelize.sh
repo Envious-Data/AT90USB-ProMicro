5cb4e4062530c81018345c42e16d54690e0863c4
EnviousData <Enviousdata@gmail.com>
2021-09-04 15:06:31 +0100

    adjusted panel and mousebites

--- panelize.sh
+++ panelize.sh
@@ -1,5 +1,8 @@
 kikit panelize \
-    --layout 'grid; rows: 2; cols: 2; space: 2mm' \
-    --tabs 'fixed; width: 5mm' \
-    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm' \
+    --layout 'grid; rows: 2; cols: 2; space: 2.5mm;' \
+    --tabs annotation \
+    --source 'tolerance: 15mm' \
+    --cuts 'mousebites; drill: 0.2mm; spacing: 0.4mm;' \
+    --framing 'railstb; width: 2.5mm; space: 2.5mm;' \
+    --post 'millradius: 1mm' \
     AT90USB-ProMicro.kicad_pcb ./_panel/AT90USB-ProMicro1.kicad_pcb
