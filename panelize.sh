9d97b891b00903c410003674a20cfcda41240114
EnviousData <Enviousdata@gmail.com>
2021-09-04 14:11:01 +0100

    correct pin hole placement and castellations

--- panelize.sh
+++ panelize.sh
@@ -2,4 +2,4 @@ kikit panelize \
     --layout 'grid; rows: 2; cols: 2; space: 2mm' \
     --tabs 'fixed; width: 5mm' \
     --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm' \
-    AT90USB-ProMicro.kicad_pcb AT90USB-ProMicro1.kicad_pcb
+    AT90USB-ProMicro.kicad_pcb ./_panel/AT90USB-ProMicro1.kicad_pcb
