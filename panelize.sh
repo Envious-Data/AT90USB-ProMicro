59c96a3d326370d2a1dc66762b7ec236ae9b65a2
EnviousData <Enviousdata@gmail.com>
2021-09-04 13:31:39 +0100

    added castellations on all pins

---
+++ panelize.sh
@@ -0,0 +1,5 @@
+kikit panelize \
+    --layout 'grid; rows: 2; cols: 2; space: 2mm' \
+    --tabs 'fixed; width: 5mm' \
+    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: 0.2mm' \
+    AT90USB-ProMicro.kicad_pcb AT90USB-ProMicro1.kicad_pcb
