12418bce359a944780281c1cd530d680aca0edb5
EnviousData <Enviousdata@gmail.com>
2021-09-04 19:01:10 +0100

    updated the panelize bash script to work under WSL.

--- panelize.sh
+++ panelize.sh
@@ -1,8 +1,1 @@
-kikit panelize \
-    --layout 'grid; rows: 2; cols: 2; space: 2.5mm;' \
-    --tabs annotation \
-    --source 'tolerance: 15mm' \
-    --cuts 'mousebites; drill: 0.2mm; spacing: 0.4mm;' \
-    --framing 'railstb; width: 2.5mm; space: 2.5mm;' \
-    --post 'millradius: 1mm' \
-    AT90USB-ProMicro.kicad_pcb ./_panel/AT90USB-ProMicro1.kicad_pcb
+kikit panelize --layout 'grid; rows: 2; cols: 2; space: 2.5mm;' --tabs annotation --source 'tolerance: 15mm' --cuts 'mousebites; drill: 0.2mm; spacing: 0.4mm;' --framing 'railstb; width: 2.5mm; space: 2.5mm;' --post 'millradius: 1mm' AT90USB-ProMicro.kicad_pcb ./_panel/AT90USB-ProMicro1.kicad_pcb
